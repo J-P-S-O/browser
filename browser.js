c54936f6b0dc004912a22e82b7debb6d7c569c55
JPSO <jpso.guerreiro@gmail.com>
2021-01-19 11:26:31 +0000

    .

--- browser.js
+++ browser.js
@@ -14,7 +14,7 @@ onload = function() {
   };
 
   document.querySelector('#home').onclick = function() {
-    navigateTo('http://www.github.com/');
+    navigateTo('https://www.google.com/');
   };
 
   document.querySelector('#reload').onclick = function() {
@@ -44,102 +44,6 @@ onload = function() {
   webview.addEventListener('did-get-redirect-request', handleLoadRedirect);
   webview.addEventListener('did-finish-load', handleLoadCommit);
 
-  // Test for the presence of the experimental <webview> zoom and find APIs.
-  if (typeof(webview.setZoom) == "function" &&
-      typeof(webview.find) == "function") {
-    var findMatchCase = false;
-
-    document.querySelector('#zoom').onclick = function() {
-      if(document.querySelector('#zoom-box').style.display == '-webkit-flex') {
-        closeZoomBox();
-      } else {
-        openZoomBox();
-      }
-    };
-
-    document.querySelector('#zoom-form').onsubmit = function(e) {
-      e.preventDefault();
-      var zoomText = document.forms['zoom-form']['zoom-text'];
-      var zoomFactor = Number(zoomText.value);
-      if (zoomFactor > 5) {
-        zoomText.value = "5";
-        zoomFactor = 5;
-      } else if (zoomFactor < 0.25) {
-        zoomText.value = "0.25";
-        zoomFactor = 0.25;
-      }
-      webview.setZoom(zoomFactor);
-    }
-
-    document.querySelector('#zoom-in').onclick = function(e) {
-      e.preventDefault();
-      increaseZoom();
-    }
-
-    document.querySelector('#zoom-out').onclick = function(e) {
-      e.preventDefault();
-      decreaseZoom();
-    }
-
-    document.querySelector('#find').onclick = function() {
-      if(document.querySelector('#find-box').style.display == 'block') {
-        document.querySelector('webview').stopFinding();
-        closeFindBox();
-      } else {
-        openFindBox();
-      }
-    };
-
-    document.querySelector('#find-text').oninput = function(e) {
-      webview.find(document.forms['find-form']['find-text'].value,
-                   {matchCase: findMatchCase});
-    }
-
-    document.querySelector('#find-text').onkeydown = function(e) {
-      if (event.ctrlKey && event.keyCode == 13) {
-        e.preventDefault();
-        webview.stopFinding('activate');
-        closeFindBox();
-      }
-    }
-
-    document.querySelector('#match-case').onclick = function(e) {
-      e.preventDefault();
-      findMatchCase = !findMatchCase;
-      var matchCase = document.querySelector('#match-case');
-      if (findMatchCase) {
-        matchCase.style.color = "blue";
-        matchCase.style['font-weight'] = "bold";
-      } else {
-        matchCase.style.color = "black";
-        matchCase.style['font-weight'] = "";
-      }
-      webview.find(document.forms['find-form']['find-text'].value,
-                   {matchCase: findMatchCase});
-    }
-
-    document.querySelector('#find-backward').onclick = function(e) {
-      e.preventDefault();
-      webview.find(document.forms['find-form']['find-text'].value,
-                   {backward: true, matchCase: findMatchCase});
-    }
-
-    document.querySelector('#find-form').onsubmit = function(e) {
-      e.preventDefault();
-      webview.find(document.forms['find-form']['find-text'].value,
-                   {matchCase: findMatchCase});
-    }
-
-    webview.addEventListener('findupdate', handleFindUpdate);
-    window.addEventListener('keydown', handleKeyDown);
-  } else {
-    var zoom = document.querySelector('#zoom');
-    var find = document.querySelector('#find');
-    zoom.style.visibility = "hidden";
-    zoom.style.position = "absolute";
-    find.style.visibility = "hidden";
-    find.style.position = "absolute";
-  }
 };
 
 function navigateTo(url) {
@@ -280,70 +184,3 @@ function handleLoadRedirect(event) {
   document.querySelector('#location').value = event.newUrl;
 }
 
-function getNextPresetZoom(zoomFactor) {
-  var preset = [0.25, 0.33, 0.5, 0.67, 0.75, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2,
-                2.5, 3, 4, 5];
-  var low = 0;
-  var high = preset.length - 1;
-  var mid;
-  while (high - low > 1) {
-    mid = Math.floor((high + low)/2);
-    if (preset[mid] < zoomFactor) {
-      low = mid;
-    } else if (preset[mid] > zoomFactor) {
-      high = mid;
-    } else {
-      return {low: preset[mid - 1], high: preset[mid + 1]};
-    }
-  }
-  return {low: preset[low], high: preset[high]};
-}
-
-function increaseZoom() {
-  var webview = document.querySelector('webview');
-  webview.getZoom(function(zoomFactor) {
-    var nextHigherZoom = getNextPresetZoom(zoomFactor).high;
-    webview.setZoom(nextHigherZoom);
-    document.forms['zoom-form']['zoom-text'].value = nextHigherZoom.toString();
-  });
-}
-
-function decreaseZoom() {
-  var webview = document.querySelector('webview');
-  webview.getZoom(function(zoomFactor) {
-    var nextLowerZoom = getNextPresetZoom(zoomFactor).low;
-    webview.setZoom(nextLowerZoom);
-    document.forms['zoom-form']['zoom-text'].value = nextLowerZoom.toString();
-  });
-}
-
-function openZoomBox() {
-  document.querySelector('webview').getZoom(function(zoomFactor) {
-    var zoomText = document.forms['zoom-form']['zoom-text'];
-    zoomText.value = Number(zoomFactor.toFixed(6)).toString();
-    document.querySelector('#zoom-box').style.display = '-webkit-flex';
-    zoomText.select();
-  });
-}
-
-function closeZoomBox() {
-  document.querySelector('#zoom-box').style.display = 'none';
-}
-
-function openFindBox() {
-  document.querySelector('#find-box').style.display = 'block';
-  document.forms['find-form']['find-text'].select();
-}
-
-function closeFindBox() {
-  var findBox = document.querySelector('#find-box');
-  findBox.style.display = 'none';
-  findBox.style.left = "";
-  findBox.style.opacity = "";
-  document.querySelector('#find-results').innerText= "";
-}
-
-function closeBoxes() {
-  closeZoomBox();
-  closeFindBox();
-}
